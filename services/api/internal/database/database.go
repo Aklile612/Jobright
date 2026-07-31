d633530d7a1452c1d31588fc3bc24c019ae4df2e
Aklile612 <aklilemente72@gmail.com>
2026-07-31 12:23:26 +0300

    Add postgres connection and migration

---
+++ services/api/internal/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/jobright/api/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect(databaseURL string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("connect postgres: %w", err)
+	}
+	return db, nil
+}
+
+func Migrate(db *gorm.DB) error {
+	return db.AutoMigrate(
+		&models.User{},
+		&models.Job{},
+		&models.Resume{},
+		&models.Application{},
+		&models.Bookmark{},
+	)
+}
